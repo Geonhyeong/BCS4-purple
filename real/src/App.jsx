5ac450c49f681465318474b86ff62be7a289b0b9
cryptojang <robert.crypto.jang@gmail.com>
2023-11-17 17:52:34 +0900

    basic layout

--- real/src/App.jsx
+++ real/src/App.jsx
@@ -1,5 +1,26 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <>
+      <header className="bg-red-100 container h-[76px]">header</header>
+      <div className="bg-orange-100  container h-[440px]">banner</div>
+      <div className="bg-yellow-100 container h-[354.38px]">welcome</div>
+      <div className="bg-green-100 container h-[1451.97px]">get started</div>
+      <div className="bg-blue-100 container h-[720px]">특징 1</div>
+      <div className="bg-blue-100 container h-[720px]">특징 2</div>
+      <div className="bg-blue-100 container h-[720px]">특징 3</div>
+      <div className="bg-blue-100 container h-[720px]">특징 4</div>
+      <div className="bg-blue-100 container h-[720px]">특징 5</div>
+      <div className="bg-purple-100 container h-[952.8px]">eth today</div>
+      <div className="container">line</div>
+      <div className="bg-red-200 container h-[591px]">join community</div>
+      <div className="bg-orange-200 container h-[1358.17px]">
+        explore contribute
+      </div>
+      <div className="container">helpful sticky button</div>
+
+      <footer className="bg-green-200 container">footer</footer>
+    </>
+  );
 };
 
 export default App;
